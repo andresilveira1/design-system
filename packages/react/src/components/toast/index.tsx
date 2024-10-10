0f7a3394d7d483f835637a848b6731e525585e3b
André Silveira <andre.silveira1silva@gmail.com>
2024-10-10 19:28:03 -0300

    style: add keyframe.

--- packages/react/src/components/toast/index.tsx
+++ packages/react/src/components/toast/index.tsx
@@ -1,10 +1,11 @@
-import { Provider, Viewport } from '@radix-ui/react-toast'
+import { Provider } from '@radix-ui/react-toast'
 
 import {
   ToastClose,
   ToastContainer,
   ToastDescription,
   ToastTitle,
+  ToastViewport,
 } from './styles'
 import { X } from 'phosphor-react'
 import { ComponentProps } from 'react'
@@ -18,9 +19,9 @@ export interface ToastProps extends ComponentProps<typeof ToastContainer> {
 export function Toast({ title, description, duration, ...props }: ToastProps) {
   return (
     <Provider>
-      <ToastContainer defaultOpen duration={duration} {...props}>
+      <ToastContainer duration={duration} {...props}>
         <ToastTitle>
-          {title}{' '}
+          {title}
           <ToastClose asChild>
             <X />
           </ToastClose>
@@ -29,7 +30,7 @@ export function Toast({ title, description, duration, ...props }: ToastProps) {
         <ToastDescription>{description}</ToastDescription>
       </ToastContainer>
 
-      <Viewport />
+      <ToastViewport />
     </Provider>
   )
 }
--- packages/react/src/components/toast/styles.ts
+++ packages/react/src/components/toast/styles.ts
@@ -1,22 +1,43 @@
 import * as Toast from '@radix-ui/react-toast'
 
-import { styled } from '../../styles'
+import { keyframes, styled } from '../../styles'
+
+const slideIn = keyframes({
+  from: {
+    transform: 'translateX(calc(100% + 25px))',
+  },
+  to: {
+    transform: 'translateX(0)',
+  },
+})
+
+const hide = keyframes({
+  from: {
+    opacity: 1,
+  },
+  to: {
+    opacity: 0,
+  },
+})
 
 export const ToastContainer = styled(Toast.Root, {
   display: 'flex',
   flexDirection: 'column',
   gap: '$1',
-
   width: '22.5rem',
   boxSizing: 'border-box',
   padding: '$3 $5',
-
   backgroundColor: '$gray800',
   borderRadius: '$sm',
   border: '1px solid $gray600',
-
   '&:focus': {
     boxShadow: '0 0 0 2px $colors$gray100',
+  },
+  '&[data-state="open"]': {
+    animation: `${slideIn} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
+  },
+  '&[data-state="closed"]': {
+    animation: `${hide} 100ms ease-in`,
   },
 })
 
@@ -54,4 +75,16 @@ export const ToastClose = styled(Toast.Close, {
   '&:hover': {
     color: '$gray100',
   },
+})
+
+export const ToastViewport = styled(Toast.Viewport, {
+  position: 'fixed',
+  bottom: 0,
+  right: 0,
+
+  listStyle: 'none',
+
+  maxWidth: '100vw',
+  margin: 0,
+  padding: '$6',
 })
